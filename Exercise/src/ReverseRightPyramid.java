0bc1dd6c191ba8f8e8ef8c6cf84f30241c1a48fb
Sneha <snehasidic87@gmail.com>
2024-06-13 12:05:18 +0530

    exercise programs

---
+++ Exercise/src/ReverseRightPyramid.java
@@ -0,0 +1,9 @@
+public class ReverseRightPyramid {
+    public static void main(String[] args) {
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+    }
+}
